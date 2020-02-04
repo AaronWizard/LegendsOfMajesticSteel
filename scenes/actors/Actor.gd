b4f30911593fc02934ed0282a60026e029f1d9de
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2020-02-03 22:51:49 -0400

    Actor pivot and player camera
    Actor.cell_pos -> Actor.cell.
    Added actor sprite pivot and offset property.
    Player actor has camera

--- scenes/actors/Actor.gd
+++ scenes/actors/Actor.gd
@@ -2,27 +2,42 @@ class_name Actor
 extends Node2D
 
 export var tile_size := Vector2(16, 16) setget set_tile_size
-var cell_pos: Vector2 setget set_cell_pos
 
+var cell: Vector2 setget set_cell
+var cell_offset: Vector2 setget set_cell_offset
+
+onready var _pivot: Position2D = get_node("Pivot")
+onready var _sprite: Sprite = get_node("Pivot/Sprite")
 
 func _ready() -> void:
-	var cell := position.snapped(tile_size) / tile_size
-	set_cell_pos(cell)
+	var new_cell := position.snapped(tile_size) / tile_size
+	set_cell(new_cell)
 
 
 func set_tile_size(new_value: Vector2) -> void:
 	tile_size = new_value
+	_set_pivot_position()
 	_set_pixel_position()
 
 
-func set_cell_pos(new_value: Vector2) -> void:
-	cell_pos = new_value
+func set_cell(new_value: Vector2) -> void:
+	cell = new_value
 	_set_pixel_position()
+
+
+func set_cell_offset(new_value: Vector2) -> void:
+	cell_offset = new_value
+	_set_pivot_position()
+
+
+func _set_pivot_position() -> void:
+	var pivot_cell_pos = cell_offset + (Vector2.ONE / 2.0)
+	_pivot.position = pivot_cell_pos * tile_size
 
 
 func _set_pixel_position() -> void:
-	position = cell_pos * tile_size
+	position = cell * tile_size
 
 
-func on_cell(cell: Vector2) -> bool:
-	return cell_pos == cell
+func on_cell(c: Vector2) -> bool:
+	return cell == c
--- scenes/ai/Player.gd
+++ scenes/ai/Player.gd
@@ -4,4 +4,4 @@ extends Controller
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("click"):
-		get_actor().cell_pos = get_map().get_mouse_cell()
+		get_actor().cell = get_map().get_mouse_cell()
