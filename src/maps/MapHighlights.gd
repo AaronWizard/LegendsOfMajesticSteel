a29f6ee914cb1d30449ab01235c6c2302b1fcf29
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2020-02-15 10:56:11 -0400

    MapHighlights method renaming

--- src/maps/MapHighlights.gd
+++ src/maps/MapHighlights.gd
@@ -10,7 +10,7 @@ enum Tiles {
 onready var _highlights: TileMap = get_node("Highlights")
 
 
-func set_walk_highlight(cells: Array) -> void:
+func set_move_highlight(cells: Array) -> void:
 	for c in cells:
 		var cell: Vector2 = c
 		_highlights.set_cellv(cell, Tiles.WALK)
