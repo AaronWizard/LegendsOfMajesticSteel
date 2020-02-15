bf62c6c897f2f04433c472354f32608f75399f03
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2020-02-15 10:57:25 -0400

    Actor pathfinding

--- src/Game.gd
+++ src/Game.gd
@@ -5,14 +5,14 @@ export var start_map_file: PackedScene = null
 onready var _map_container := get_node("Map")
 onready var _map_highlights: MapHighlights = get_node("MapHighlights")
 onready var _camera: GameCamera = get_node("GameCamera")
-onready var _turn_manager := TurnManager.new()
+onready var _turn_manager: TurnManager = get_node("TurnManager")
 
 
 func _ready() -> void:
 	if start_map_file:
 		load_map(start_map_file)
 
-	_turn_manager.start(get_current_map().get_actors(), _camera)
+	_turn_manager.start(get_current_map())
 
 
 func get_current_map() -> Map:
@@ -30,3 +30,15 @@ func load_map(map_file: PackedScene) -> void:
 	_map_container.add_child(test)
 
 	_camera.set_bounds(test.get_pixel_rect())
+
+
+func _on_TurnManager_followed_actor(actor: Actor) -> void:
+	_camera.follow_actor(actor)
+
+
+func _on_TurnManager_set_movement_range(move_range: Array) -> void:
+	_map_highlights.set_move_highlight(move_range)
+
+
+func _on_TurnManager_cleared_map_highlights() -> void:
+	_map_highlights.clear()
--- src/TurnManager.gd
+++ src/TurnManager.gd
@@ -1,11 +1,18 @@
 class_name TurnManager
+extends Node
+
+signal followed_actor(actor)
+signal set_movement_range(move_range)
+signal cleared_map_highlights
 
 var running := false
 
 var _current_index := 0
 
 
-func start(actors: Array, camera: GameCamera) -> void:
+func start(map: Map) -> void:
+	var actors := map.get_actors()
+
 	running = true
 	_current_index = 0
 
@@ -13,11 +20,20 @@ func start(actors: Array, camera: GameCamera) -> void:
 		var actor := actors[_current_index] as Actor
 		var controller := actor.controller
 		if controller:
-			camera.follow_actor(actor)
+			_do_pathfinding(actor, map)
+
+			emit_signal("followed_actor", actor)
+
 			controller.determine_action()
 			var action: Action = yield(controller, "determined_action")
+			emit_signal("cleared_map_highlights")
 			if action:
 				action.start()
 				yield(action, "finished")
 
 		_current_index = (_current_index + 1) % actors.size()
+
+
+func _do_pathfinding(actor: Actor, map: Map) -> void:
+	var move_range := BreadthFirstSearch.find_move_range(actor, map)
+	emit_signal("set_movement_range", move_range)
