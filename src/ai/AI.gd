4c3665a140e320e5ecac039876f1b2230cf80960
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2020-02-23 13:03:28 -0400

    Spacing. Will use RNG for AI.

--- src/ai/AI.gd
+++ src/ai/AI.gd
@@ -1,11 +1,21 @@
 class_name AI
 extends Controller
 
+onready var _random := RandomNumberGenerator.new()
+
+
+func _ready() -> void:
+	._ready()
+	_random.randomize()
+
+
 func connect_to_gui(_gui: BattleGUI) -> void:
 	pass
 
+
 func disconnect_from_gui(_gui: BattleGUI) -> void:
 	pass
+
 
 func determine_action() -> void:
 	#print("Controller: Must implement determine_action()")
