e93bc7120920af2c0fd7d927d1baab1be4979ed5
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2020-02-22 22:36:29 -0400

    Dragging vs clicking based on mouse speed instead of mouse-down time

--- src/game/BattleGUI.gd
+++ src/game/BattleGUI.gd
@@ -2,36 +2,37 @@ class_name BattleGUI
 extends CanvasLayer
 
 signal mouse_dragged(relative)
+signal mouse_clicked(position)
 signal wait_pressed
+
+const MIN_DRAG_SPEED_SQUARED = 8^2
 
 var buttons_visible := false setget set_buttons_visible
 
-var _can_drag := false
+var _mouse_down := false
+var _dragging := false
 
-onready var _drag_timer: Timer = $DragTimer
 onready var _all_buttons: Node = $Buttons
 
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.button_index == BUTTON_LEFT:
-			if event.pressed:
-				_drag_timer.start()
-			else:
-				_drag_timer.stop()
-				_can_drag = false
-	elif event is InputEventMouseMotion and _can_drag:
-		emit_signal("mouse_dragged", event.relative)
+			if not event.pressed and _mouse_down and not _dragging:
+				print("clicked")
+				emit_signal("mouse_clicked", event.position)
+			_mouse_down = event.pressed
+	elif event is InputEventMouseMotion:
+		_dragging = _mouse_down \
+				and (event.speed.length_squared() >= MIN_DRAG_SPEED_SQUARED)
+		if _dragging:
+			emit_signal("mouse_dragged", event.relative)
 
 
 func set_buttons_visible(value: bool) -> void:
 	for b in _all_buttons.get_children():
 		var button: Button = b
 		button.visible = value
-
-
-func _on_DragTimer_timeout() -> void:
-	_can_drag = true
 
 
 func _on_Wait_pressed() -> void:
