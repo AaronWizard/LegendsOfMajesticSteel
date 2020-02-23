900146f2ba6e149eca220c1ea24a921fe0cbeab3
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2020-02-23 13:13:46 -0400

    BattleStats does more heavy lifting in determining where actor may move

--- src/actors/BattleStats.gd
+++ src/actors/BattleStats.gd
@@ -2,7 +2,8 @@ class_name BattleStats
 extends Node
 
 var finished := false
-var walk_cells := []
+var move_range := []
+var enterable_cells := []
 
 onready var _walk_grid := AStar2D.new()
 
@@ -11,21 +12,23 @@ func start_turn() -> void:
 	finished = false
 
 	var map: Map = _get_actor().map as Map
-	walk_cells = BreadthFirstSearch.find_move_range(_get_actor(), map)
-	_build_walk_grid()
+	move_range = BreadthFirstSearch.find_move_range(_get_actor(), map)
+	_update_move_range()
 
 
 func get_walk_path(cell_to: Vector2) -> Array:
 	var result := []
 
-	var end_point := _walk_path_point(cell_to)
-	if end_point > -1:
-		var start_cell := _get_actor().cell
-		var start_point := _walk_path_point(start_cell)
-		assert(start_point > -1)
+	if (cell_to != _get_actor().cell) and (cell_to in enterable_cells):
+		var end_point := _walk_path_point(cell_to)
+		if end_point > -1:
+			var start_cell := _get_actor().cell
+			var start_point := _walk_path_point(start_cell)
+			assert(start_point > -1)
 
-		result = _walk_grid.get_point_path(start_point, end_point)
-		result.pop_front() # Remove starting cell
+			result = _walk_grid.get_point_path(start_point, end_point)
+			result.pop_front() # Remove starting cell
+		assert(result.size() > 0)
 
 	return result
 
@@ -34,14 +37,24 @@ func _get_actor() -> Actor:
 	return owner as Actor
 
 
-func _build_walk_grid() -> void:
+func _get_map() -> Map:
+	return _get_actor().map
+
+func _update_move_range() -> void:
+	enterable_cells.clear()
 	_walk_grid.clear()
-	for c in walk_cells:
+
+	for c in move_range:
 		var cell: Vector2 = c
+
 		_walk_grid.add_point(
 			_walk_grid.get_available_point_id(),
 			cell
 		)
+
+		if _get_map().actor_can_enter_cell(_get_actor(), cell):
+			enterable_cells.append(cell)
+
 	for p in _walk_grid.get_points():
 		var point: int = p
 		var cell := _walk_grid.get_point_position(point)
