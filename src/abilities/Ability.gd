697eb96bca961e87b03e9164c1c938e078210f33
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2020-02-28 22:30:02 -0400

    Working on actor abilities

---
+++ src/abilities/Ability.gd
@@ -0,0 +1,30 @@
+class_name Ability
+extends Node
+
+signal finished
+
+
+func get_actor() -> Actor:
+	return owner as Actor
+
+
+func get_map() -> Map:
+	return get_actor().map
+
+
+func get_battle_stats() -> BattleStats:
+	return get_actor().battle_stats
+
+
+func get_range(source_cell: Vector2) -> Array:
+	print("Ability must implement get_range()")
+	return []
+
+
+func get_aoe(source_cell: Vector2, target_cell: Vector2) -> Array:
+	return [target_cell]
+
+
+func start(target: Vector2) -> void:
+	print("Ability must implement start()")
+	emit_signal("finished")
---
+++ src/actions/AbilityAction.gd
@@ -0,0 +1,17 @@
+class_name AbilityAction
+extends Action
+
+var ability: Ability
+var target: Vector2
+
+
+func _init(new_actor: Actor, new_map: Map, new_ability: Ability, \
+		new_target: Vector2).(new_actor, new_map) -> void:
+	ability = new_ability
+	target = new_target
+
+
+func start() -> void:
+	ability.start(target)
+	yield(ability, "finished")
+	emit_signal("finished")
