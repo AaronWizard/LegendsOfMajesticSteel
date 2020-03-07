eb4ea6cf091fc0ccd9740b4af17b8ae76cf42a52
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2020-03-07 11:35:25 -0400

    "get_current_*" methods for Ability that uses actor's current cell

--- src/abilities/Ability.gd
+++ src/abilities/Ability.gd
@@ -16,14 +16,26 @@ func get_battle_stats() -> BattleStats:
 	return get_actor().battle_stats
 
 
+func get_current_range() -> Array:
+	return get_range(get_actor().cell)
+
+
 func get_range(source_cell: Vector2) -> Array:
 	print("Ability must implement get_range()")
-	return []
+	return [source_cell]
+
+
+func get_current_valid_targets() -> Array:
+	return get_valid_targets(get_actor().cell)
 
 
 func get_valid_targets(source_cell: Vector2) -> Array:
 	print("Ability must get_valid_targets get_range()")
-	return []
+	return [source_cell]
+
+
+func get_current_aoe(target_cell: Vector2) -> Array:
+	return get_aoe(get_actor().cell, target_cell)
 
 
 func get_aoe(source_cell: Vector2, target_cell: Vector2) -> Array:
