bfc4025402b1f3b6835cb1432d61d94dffe60e6e
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2020-04-21 14:33:44 -0300

    Updated stamina bar graphics

--- src/ui/StaminaBar.gd
+++ src/ui/StaminaBar.gd
@@ -1,34 +1,53 @@
-tool
+class_name StaminaBar
 extends Node2D
 
-enum _CellState { STAMINA, DAMAGE, HEAL }
+const MAX_STAMINA := 20
 
-export(int, 0, 20) var current := 20 setget set_current
+export(int, 0, 20) var current := MAX_STAMINA setget set_current
 export var modifier := 0 setget set_modifier
 
 onready var _cells := $Cells as Node
+
+onready var _damage := $Damage as Node2D
+onready var _healing := $Healing as Node2D
 
 func set_current(value: int) -> void:
 	current = value
 	_update_cells()
 
+
 func set_modifier(value: int) -> void:
 	modifier = value
 	_update_cells()
 
+
 func _update_cells() -> void:
 	if _cells:
-		var mod_current := current + modifier
-		var mod_min := min(current, mod_current)
-		var mod_max := max(current, mod_current)
+		for c in _cells.get_children():
+			var cell := c as Node2D
+			cell.visible = cell.get_index() < current
 
-		for i in range(_cells.get_child_count()):
-			var cell := _cells.get_child(i) as AnimatedSprite
-			cell.visible = i < mod_max
+	if _damage:
+		if modifier < 0:
+			var min_value := int(max(current + modifier, 0))
+			var max_value := current
+			_update_modifier_cells(_damage, min_value, max_value)
+		else:
+			_damage.visible = false
 
-			if i < mod_min:
-				cell.frame = _CellState.STAMINA
-			elif (i >= mod_min) and (i < current):
-				cell.frame = _CellState.DAMAGE
-			elif (i >= current) and (i < mod_max):
-				cell.frame = _CellState.HEAL
+	if _healing:
+		if modifier > 0:
+			var min_value := current
+			var max_value := int(min(current + modifier, MAX_STAMINA))
+			_update_modifier_cells(_healing, min_value, max_value)
+		else:
+			_healing.visible = false
+
+
+func _update_modifier_cells(modifier_cells: Node2D, min_value: int, max_value: int) -> void:
+	modifier_cells.visible = true
+
+	for c in modifier_cells.get_children():
+		var cell := c as Node2D
+		var index := cell.get_index() + 1
+		cell.visible = (index > min_value) and (index <= max_value)
