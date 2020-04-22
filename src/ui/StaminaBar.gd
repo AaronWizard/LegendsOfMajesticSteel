31a86e0703b9e3db02451f2e9200ce4dc50f2422
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2020-04-22 20:50:00 -0300

    Animated stamina bar updates

--- src/ui/StaminaBar.gd
+++ src/ui/StaminaBar.gd
@@ -1,22 +1,36 @@
 class_name StaminaBar
 extends Node2D
 
-export(int, 0, 20) var current := Constants.MAX_STAMINA setget set_current
-export var modifier := 0 setget set_modifier
+signal animation_finished
+
+var current := Constants.MAX_STAMINA setget set_current
+var modifier := 0 setget set_modifier
 
 onready var _cells := $Cells as Node
 
 onready var _damage := $Damage as Node2D
 onready var _healing := $Healing as Node2D
 
+onready var _cell_change_delay := $CellChangeDelay as Timer
+onready var _cell_change := $CellChange as Timer
+
+
 func set_current(value: int) -> void:
-	current = value
+	current = int(clamp(value, 0, Constants.MAX_STAMINA))
 	_update_cells()
 
 
 func set_modifier(value: int) -> void:
-	modifier = value
+	modifier = int(clamp(value, -current, Constants.MAX_STAMINA - current))
 	_update_cells()
+
+
+func animate_change(delta: int) -> void:
+	if delta != 0:
+		set_modifier(modifier + delta)
+		if modifier != 0:
+			_cell_change.stop()
+			_cell_change_delay.start()
 
 
 func _update_cells() -> void:
@@ -27,7 +41,8 @@ func _update_cells() -> void:
 
 	if _damage:
 		if modifier < 0:
-			var min_value := int(max(current + modifier, 0))
+			assert(current + modifier >= 0)
+			var min_value := current + modifier
 			var max_value := current
 			_update_modifier_cells(_damage, min_value, max_value)
 		else:
@@ -35,9 +50,9 @@ func _update_cells() -> void:
 
 	if _healing:
 		if modifier > 0:
+			assert(current + modifier <= Constants.MAX_STAMINA)
 			var min_value := current
-			var max_value := int(min(
-					current + modifier, Constants.MAX_STAMINA))
+			var max_value := current + modifier
 			_update_modifier_cells(_healing, min_value, max_value)
 		else:
 			_healing.visible = false
@@ -51,3 +66,22 @@ func _update_modifier_cells(modifier_cells: Node2D,
 		var cell := c as Node2D
 		var index := cell.get_index() + 1
 		cell.visible = (index > min_value) and (index <= max_value)
+
+
+func _on_CellChangeDelay_timeout() -> void:
+	assert(modifier != 0)
+	assert((current >= 0) && (current <= Constants.MAX_STAMINA))
+	_cell_change.start()
+
+
+func _on_CellChange_timeout() -> void:
+	assert(modifier != 0)
+	assert((current >= 0) && (current <= Constants.MAX_STAMINA))
+
+	var delta := int(sign(modifier))
+	set_modifier(modifier - delta)
+	set_current(current + delta)
+
+	if modifier == 0:
+		_cell_change.stop()
+		emit_signal("animation_finished")
