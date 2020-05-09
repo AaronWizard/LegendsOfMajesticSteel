31ee124d4aa23c9d8cbbd09d24a64ac72307568a
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2020-05-09 09:10:00 -0300

    Fix how camera follows actors

--- src/game/GameCamera.gd
+++ src/game/GameCamera.gd
@@ -1,6 +1,8 @@
 class_name GameCamera
 extends Camera2D
 
+var _current_actor: Actor = null
+
 
 func set_bounds(rect: Rect2) -> void:
 	self.limit_left = int(rect.position.x)
@@ -28,13 +30,21 @@ func drag(relative: Vector2) -> void:
 
 
 func follow_actor(actor: Actor) -> void:
+	stop_following_actor()
+
 	drag_margin_h_enabled = true
 	drag_margin_v_enabled = true
 	smoothing_enabled = true
-	actor.remote_transform.remote_path = get_path()
+
+	_current_actor = actor
+	_current_actor.remote_transform.remote_path = get_path()
 
 
 func stop_following_actor() -> void:
+	if _current_actor:
+		_current_actor.remote_transform.remote_path = NodePath()
+		_current_actor = null
+
 	drag_margin_h_enabled = false
 	drag_margin_v_enabled = false
 	smoothing_enabled = false
