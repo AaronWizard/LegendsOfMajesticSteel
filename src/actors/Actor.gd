bf8d9e4b174471cf46b388ac058db2a6b4d924c7
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2020-06-14 10:16:08 -0300

    Removed "tile_size" property from Actor

--- src/actors/Actor.gd
+++ src/actors/Actor.gd
@@ -14,8 +14,6 @@ const _MOVE_ANIMS = {
 	Directions.WEST: "actor_move_west"
 }
 
-export var tile_size := Vector2(Constants.TILE_SIZE, Constants.TILE_SIZE) \
-		setget set_tile_size
 export var cell_offset: Vector2 setget set_cell_offset, get_cell_offset
 
 export(Faction) var faction := Faction.ENEMY
@@ -41,49 +39,36 @@ onready var _anim := $AnimationPlayer as AnimationPlayer
 
 
 func _ready() -> void:
-	var new_cell := position.snapped(tile_size) / tile_size
-	set_tile_size(tile_size)
+	_center.position = Constants.TILE_SIZE_V / 2
+
+	var new_cell := \
+			position.snapped(Constants.TILE_SIZE_V) / Constants.TILE_SIZE_V
 	set_cell(new_cell)
 
 
 func _draw() -> void:
 	if Engine.editor_hint:
-		var rect := Rect2(Vector2.ZERO, tile_size)
+		var rect := Rect2(Vector2.ZERO, Constants.TILE_SIZE_V)
 		draw_rect(rect, Color.magenta, false)
 
 
-func set_tile_size(new_value: Vector2) -> void:
-	# Cell and cell offset based on old tile_size
-	var old_cell := get_cell()
-	var old_cell_offset := get_cell_offset()
-
-	tile_size = new_value
-	if _center:
-		_center.position = tile_size / 2
-
-	set_cell(old_cell)
-	set_cell_offset(old_cell_offset)
-
-	update()
-
-
 func set_cell(new_value: Vector2) -> void:
-	position = new_value * tile_size
+	position = new_value * Constants.TILE_SIZE_V
 
 
 func get_cell() -> Vector2:
-	return position / tile_size
+	return position / Constants.TILE_SIZE_V
 
 
 func set_cell_offset(new_value: Vector2) -> void:
 	if _offset:
-		_offset.position = new_value * tile_size
+		_offset.position = new_value * Constants.TILE_SIZE_V
 
 
 func get_cell_offset() -> Vector2:
 	var result := Vector2.ZERO
 	if _offset:
-		result = _offset.position / tile_size
+		result = _offset.position / Constants.TILE_SIZE_V
 	return result
 
 
