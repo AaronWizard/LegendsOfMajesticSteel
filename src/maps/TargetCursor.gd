4d15815d26916371b176d5804df75331c5ba86f2
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2020-07-24 22:47:04 -0300

    Target cursors can be centered

--- src/maps/TargetCursor.gd
+++ src/maps/TargetCursor.gd
@@ -1,7 +1,9 @@
+tool
 class_name TargetCursor
 extends Node2D
 
 export var tile_size: Vector2 = Vector2(1, 1) setget set_tile_size
+export var centered: bool = false setget set_centered
 
 onready var _corner_nw := $CornerNW as Node2D
 onready var _corner_ne := $CornerNE as Node2D
@@ -10,10 +12,34 @@ onready var _corner_sw := $CornerSW as Node2D
 
 func _ready() -> void:
 	set_tile_size(tile_size)
+	set_centered(centered)
 
 
 func set_tile_size(new_value: Vector2) -> void:
 	tile_size = new_value
-	_corner_ne.position.x = tile_size.x * Constants.TILE_SIZE
-	_corner_se.position = tile_size * Constants.TILE_SIZE
-	_corner_sw.position.y = tile_size.y * Constants.TILE_SIZE
+	_arrange_corners()
+
+
+func set_centered(new_value: bool) -> void:
+	centered = new_value
+	_arrange_corners()
+
+
+func _arrange_corners() -> void:
+	if not _corner_ne:
+		return
+
+	if centered:
+		var half_size := (tile_size / 2) * Constants.TILE_SIZE
+
+		_corner_ne.position = Vector2( 1, -1) * half_size
+		_corner_se.position = Vector2( 1,  1) * half_size
+		_corner_sw.position = Vector2(-1,  1) * half_size
+		_corner_nw.position = Vector2(-1, -1) * half_size
+	else:
+		var size := tile_size * Constants.TILE_SIZE
+
+		_corner_ne.position = Vector2(1, 0) * size
+		_corner_se.position = Vector2(1, 1) * size
+		_corner_sw.position = Vector2(0, 1) * size
+		_corner_nw.position = Vector2.ZERO
