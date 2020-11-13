40adee04e8faf9ac5789658d0f5acfb4a9ce9a81
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2020-11-13 12:35:39 -0500

    Display max stamina in actor status panel

--- src/ui/ActorStatusPanel.gd
+++ src/ui/ActorStatusPanel.gd
@@ -5,10 +5,13 @@ signal portrait_pressed
 
 onready var _portrait := $HBoxContainer/PortraitButton as Button
 onready var _name := $HBoxContainer/VBoxContainer/Name as Label
-onready var _stamina := $HBoxContainer/VBoxContainer/HBoxContainer/ \
-		Stamina as Range
-onready var _stamina_number := $HBoxContainer/VBoxContainer/HBoxContainer/ \
-		StaminaNumber as Label
+
+onready var _stamina_bar := $HBoxContainer/VBoxContainer/HBoxContainer/Stamina \
+		as Range
+onready var _current_stamina := $HBoxContainer/VBoxContainer/HBoxContainer/ \
+		HBoxContainer/CurrentStamina as Label
+onready var _max_stamina := $HBoxContainer/VBoxContainer/HBoxContainer/ \
+		HBoxContainer/MaxStamina as Label
 
 
 func set_actor(actor: Actor) -> void:
@@ -16,16 +19,21 @@ func set_actor(actor: Actor) -> void:
 	_portrait.icon = actor.portrait
 	_portrait.disabled = false
 	_name.text = actor.character_name
-	_stamina.value = actor.battle_stats.stamina
-	_stamina_number.text = str(actor.battle_stats.stamina)
+
+	_stamina_bar.max_value = actor.stats.max_stamina
+	_stamina_bar.value = actor.battle_stats.stamina
+
+	_max_stamina.text = str(actor.stats.max_stamina)
+	_current_stamina.text = str(actor.battle_stats.stamina)
 
 
 func clear() -> void:
 	_portrait.icon = null
 	_portrait.disabled = true
 	_name.text = ""
-	_stamina.value = 0
-	_stamina_number.text = ""
+	_stamina_bar.value = 0
+	_current_stamina.text = ""
+	_max_stamina.text = ""
 
 
 func _on_PortraitButton_pressed() -> void:
