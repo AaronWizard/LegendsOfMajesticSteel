d16db78ab5fe39698b8fda96abc6ea4bf49712f5
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2020-11-24 12:58:46 -0500

    Refactor melee attack

--- src/abilities/effects/MeleeAttackEffect.gd
+++ src/abilities/effects/MeleeAttackEffect.gd
@@ -10,55 +10,28 @@ func start(target: Vector2, source_actor: Actor, map: Map) -> void:
 	var dir := target - source_actor.cell
 	var target_actor := map.get_actor_on_cell(target)
 
-	# warning-ignore:return_value_discarded
-	source_actor.connect("attack_hit", self, "_attack_hit",
-			[source_actor, target_actor, dir], CONNECT_ONESHOT)
-
-	_running_anims = 1
-	# warning-ignore:return_value_discarded
-	source_actor.connect("attack_finished", self, "_anim_finished", [],
-			CONNECT_ONESHOT)
-
+	_connect_actor_signal(source_actor, "attack_finished")
 	source_actor.animate_attack(dir)
+	yield(source_actor, "attack_hit")
 
+	target_actor.battle_stats.modify_stamina(-source_actor.stats.attack)
 
-func _attack_hit(source: Actor, target: Actor, dir: Vector2) -> void:
-	target.battle_stats.modify_stamina(-source.stats.attack)
-
-	if target.battle_stats.is_alive:
-		_connect_staimina_finished(target)
-		_connect_react_finished(target)
-		target.animate_hit(dir)
+	if target_actor.battle_stats.is_alive:
+		_connect_actor_signal(target_actor, "stamina_animation_finished")
+		_connect_actor_signal(target_actor, "hit_reaction_finished")
+		target_actor.animate_hit(dir)
 	else:
-		_connect_died(target)
-		target.animate_death(dir)
+		_connect_actor_signal(target_actor, "died")
+		target_actor.animate_death(dir)
 
 
-func _connect_staimina_finished(actor: Actor) -> void:
+func _connect_actor_signal(actor: Actor, signal_name: String) -> void:
 	_running_anims += 1
 	# warning-ignore:return_value_discarded
-	actor.connect("stamina_animation_finished", self, "_anim_finished", [],
-			CONNECT_ONESHOT)
-
-
-func _connect_react_finished(actor: Actor) -> void:
-	_running_anims += 1
-	# warning-ignore:return_value_discarded
-	actor.connect("hit_reaction_finished", self, "_anim_finished", [],
-			CONNECT_ONESHOT)
-
-
-func _connect_died(actor: Actor) -> void:
-	_running_anims += 1
-	# warning-ignore:return_value_discarded
-	actor.connect("died", self, "_anim_finished", [], CONNECT_ONESHOT)
+	actor.connect(signal_name, self, "_anim_finished", [], CONNECT_ONESHOT)
 
 
 func _anim_finished() -> void:
-	_finish_anim()
-
-
-func _finish_anim() -> void:
 	_running_anims -= 1
 	if _running_anims == 0:
 		emit_signal("finished")
