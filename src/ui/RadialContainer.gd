3e5ca8b868cba90167fd5c0943d2b6c59edf3dc9
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2020-12-16 18:07:59 -0500

    Fixes for RadialContainer. Added base rotation setting to RadialContainer.

--- src/ui/RadialContainer.gd
+++ src/ui/RadialContainer.gd
@@ -3,22 +3,26 @@ class_name RadialContainer
 extends Container
 
 
-func _notification(what: int) -> void:
-	if what == NOTIFICATION_SORT_CHILDREN:
-		_arrange_children()
+export(float, 0, 359) var base_rotation := 0.0 setget set_base_rotation # Degrees
+
+
+func set_base_rotation(new_value: float) -> void:
+	base_rotation = new_value
+	_arrange_children()
 
 
 func _arrange_children() -> void:
 	if get_children().size() > 0:
 		var angle_offset := (2 * PI) / get_children().size()
-		var angle := 0.0 # Radians
+		var angle := deg2rad(base_rotation) # Radians
 
-		var max_child_size := _max_child_size() / 2
+		var max_child_half_size := _max_child_size() / 2
 		var center := rect_size / 2
-		var radius := min(center.x - max_child_size.x, center.y - max_child_size.y)
+		var circle_size := center - max_child_half_size
 
 		for c in get_children():
-			var circle_pos := Vector2(radius, 0).rotated(angle) + center
+			var circle_pos := (Vector2(1, 0).rotated(angle) * circle_size) \
+					+ center
 			angle += angle_offset
 
 			var control := c as Control
@@ -32,3 +36,7 @@ func _max_child_size() -> Vector2:
 		result.x = max(result.x, control.rect_size.x)
 		result.y = max(result.y, control.rect_size.y)
 	return result
+
+
+func _on_RadialContainer_sort_children() -> void:
+	_arrange_children()
