4ecae8b55b4841d67193fb37efb7167dfa134007
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2021-01-23 14:51:11 -0500

    Fix how movement ranges are displayed for multi-tile actors

--- src/control/RangeData.gd
+++ src/control/RangeData.gd
@@ -1,8 +1,10 @@
 class_name RangeData
 
-var _base_move_range: Dictionary
+var _visible_move_range: Dictionary
 var _true_move_range: Dictionary
+
 var _walk_grid: AStar2D
+
 var _targeting_data_set: Dictionary
 var _valid_source_cells: Dictionary
 
@@ -15,30 +17,28 @@ static func walk_path_point(walk_grid: AStar2D, cell: Vector2) -> int:
 
 
 func get_visible_move_range() -> Array:
-	return _base_move_range.keys()
+	return _visible_move_range.keys()
 
 
 func get_move_range() -> Array:
 	return _true_move_range.keys()
 
 
-func can_move_to_cell(cell: Vector2) -> bool:
-	return _true_move_range.has(cell)
-
-
 func get_walk_path(start: Vector2, end: Vector2) -> Array:
 	var result := []
 
-	if (start != end) and _true_move_range.has(start) \
-			and _true_move_range.has(end):
-		var end_point := _walk_path_point(end)
-		if end_point > -1:
-			var start_point := _walk_path_point(start)
-			assert(start_point > -1)
+	if _visible_move_range.has(end):
+		var true_end := _visible_move_range[end] as Vector2
+		if (start != true_end) and _true_move_range.has(start) \
+				and _true_move_range.has(true_end):
+			var end_point := _walk_path_point(true_end)
+			if end_point > -1:
+				var start_point := _walk_path_point(start)
+				assert(start_point > -1)
 
-			result = _walk_grid.get_point_path(start_point, end_point)
-			result.pop_front() # Remove starting cell
-		assert(result.size() > 0)
+				result = _walk_grid.get_point_path(start_point, end_point)
+				result.pop_front() # Remove starting cell
+			assert(result.size() > 0)
 
 	return result
 
--- src/control/RangeDataFactory.gd
+++ src/control/RangeDataFactory.gd
@@ -3,14 +3,17 @@ class_name RangeDataFactory
 
 static func create_range_data(actor: Actor, map: Map) -> RangeData:
 	var base_move_range := _create_base_move_range(actor, map)
+	var visible_move_range := _create_visible_move_range(base_move_range, actor)
 	var true_move_range := _create_true_move_range(base_move_range, actor, map)
+
 	var walk_grid := _create_walk_grid(base_move_range)
+
 	var targeting_data_set := _create_targeting_data_set(
 			true_move_range, actor, map)
 	var valid_source_cells := _create_valid_source_cells(targeting_data_set)
 
 	var result := RangeData.new()
-	result._base_move_range = base_move_range
+	result._visible_move_range = visible_move_range
 	result._true_move_range = true_move_range
 	result._walk_grid = walk_grid
 	result._targeting_data_set = targeting_data_set
@@ -32,13 +35,33 @@ static func _create_base_move_range(actor: Actor, map: Map) -> Dictionary:
 	return result
 
 
+# Includes cells outside the actor's true move range but would be covered by
+# the actor if it's a multi-tile actor. Visible cells are keys and their
+# corresponding origin cells are the values.
+static func _create_visible_move_range(base_move_range: Dictionary,
+		actor: Actor) -> Dictionary:
+	var result := {}
+
+	for oc in base_move_range:
+		var origin_cell := oc as Vector2
+		result[origin_cell] = origin_cell
+
+		var covered := actor.get_covered_cells_at_cell(origin_cell)
+		for cc in covered:
+			var covered_cell := cc as Vector2
+			if not result.has(covered_cell):
+				result[covered_cell] = origin_cell
+
+	return result
+
+
 # Cells the actor can actually occupy
 static func _create_true_move_range(base_move_range: Dictionary,
 		actor: Actor, map: Map) -> Dictionary:
 	var result := {}
 
 	for c in base_move_range:
-		var cell: Vector2 = c
+		var cell := c as Vector2
 		if map.actor_can_enter_cell(actor, cell):
 			result[cell] = true
 
@@ -49,7 +72,7 @@ static func _create_walk_grid(move_range: Dictionary) -> AStar2D:
 	var result := AStar2D.new()
 
 	for c in move_range:
-		var cell: Vector2 = c
+		var cell := c as Vector2
 		result.add_point(result.get_available_point_id(), cell)
 	_init_walk_grid_paths(result)
 
@@ -58,10 +81,10 @@ static func _create_walk_grid(move_range: Dictionary) -> AStar2D:
 
 static func _init_walk_grid_paths(walk_grid: AStar2D) -> void:
 	for p in walk_grid.get_points():
-		var point: int = p
+		var point := p as int
 		var cell := walk_grid.get_point_position(point)
 		for d in Directions.get_all_directions():
-			var dir: Vector2 = d
+			var dir := d as Vector2
 			var adj_cell := dir + cell
 
 			var adj_point := RangeData.walk_path_point(walk_grid, adj_cell)
@@ -85,7 +108,7 @@ static func _create_targeting_data_set(move_range: Dictionary,
 			var targeting_data := skill.get_targeting_data(
 					source_cell, actor, map)
 
-			var key = Vector3(source_cell.x, source_cell.y, skill_index)
+			var key := Vector3(source_cell.x, source_cell.y, skill_index)
 			result[key] = targeting_data
 
 	return result
