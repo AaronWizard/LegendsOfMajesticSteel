5bdadb1c66d9432619c86bbd2cc046503d6556ec
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2021-02-27 23:28:55 -0500

    Use ShowProjectile.between_actors in projectile attack effect

--- src/skills/effects/ProjectileAttackEffect.gd
+++ src/skills/effects/ProjectileAttackEffect.gd
@@ -10,8 +10,8 @@ func run(target_cell: Vector2, _aoe: Array, source_actor: Actor, map: Map) \
 	var target_actor := map.get_actor_on_cell(target_cell)
 	var dir := source_actor.center_cell.direction_to(target_actor.center_cell)
 
-	var projectile_effect := ShowProjectile.new(projectile_scene, map,
-			Vector2.ZERO, Vector2.ZERO, source_actor, target_actor)
+	var projectile_effect := ShowProjectile.between_actors(
+			projectile_scene, map, source_actor, target_actor)
 	projectile_effect.children.append(
 		DamageActor.new(
 			target_actor, map, source_actor.stats.attack, dir
