196cc34f1d92aae41d4943605da536f8d860bca9
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2021-03-05 20:59:07 -0500

    Mouse cursor

---
+++ src/ui/MouseCursor.gd
@@ -0,0 +1,19 @@
+extends Sprite
+
+
+func _ready() -> void:
+	position = get_global_mouse_position()
+	_set_mouse_on_screen(position)
+
+
+func _process(_delta: float) -> void:
+	position = get_global_mouse_position()
+	_set_mouse_on_screen(position)
+
+
+func _set_mouse_on_screen(mouse_pos: Vector2) -> void:
+	var rect := get_viewport().get_visible_rect()
+	if rect.has_point(mouse_pos):
+		Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+	else:
+		Input.set_mouse_mode(Input.MOUSE_MODE_VISIBLE)
