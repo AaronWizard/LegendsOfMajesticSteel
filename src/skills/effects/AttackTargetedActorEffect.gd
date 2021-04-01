ea8a6eea3df54b65aa917a030022fe2aeb747091
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2021-04-01 18:22:54 -0400

    AttackTargetedActorEffect and MeleeAttack

---
+++ src/skills/effects/AttackTargetedActorEffect.gd
@@ -0,0 +1,35 @@
+class_name AttackTargetedActorEffect
+extends SkillEffect
+
+export var use_direction := true
+
+
+func get_aoe(target_cell: Vector2, _source_cell: Vector2, _source_actor: Actor,
+		map: Map) -> Array:
+	var actor := map.get_actor_on_cell(target_cell)
+	return actor.covered_cells
+
+
+func predict_damage(target_cell: Vector2, _source_cell: Vector2,
+		source_actor: Actor, map: Map) -> Dictionary:
+	var actor := map.get_actor_on_cell(target_cell)
+	var damage := actor.stats.damage_from_attack(source_actor.stats.attack)
+	return { actor: -damage }
+
+
+func run(target_cell: Vector2, source_actor: Actor, map: Map) -> void:
+	var actor := map.get_actor_on_cell(target_cell)
+	var damage := actor.stats.damage_from_attack(source_actor.stats.attack)
+
+	var direction := Vector2.ZERO
+	if use_direction:
+		direction = actor.center_cell - source_actor.center_cell
+
+	actor.stats.take_damage(damage)
+
+	if actor.is_alive:
+		actor.animate_hit(direction)
+		yield(actor, "hit_reaction_finished")
+	else:
+		actor.animate_death(direction)
+		yield(actor, "died")
