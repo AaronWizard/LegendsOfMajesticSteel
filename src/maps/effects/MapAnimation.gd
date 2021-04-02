9d329550aabda39ecb62feceedf28348608e973f
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2021-04-01 21:51:02 -0400

    Fixes for MapAnimation and shine strike swoosh

--- src/maps/effects/MapAnimation.gd
+++ src/maps/effects/MapAnimation.gd
@@ -5,6 +5,7 @@ extends TileObject
 signal finished
 
 export var anim_rotation: float setget set_anim_rotation, get_anim_rotation
+export var wait_for_sound := false
 
 onready var _anim := $Center/Offset/AnimatedSprite as AnimatedSprite
 onready var _sound := $Sound as AudioStreamPlayer
@@ -12,7 +13,7 @@ onready var _sound := $Sound as AudioStreamPlayer
 
 func _ready() -> void:
 	if not Engine.editor_hint:
-		_anim.play()
+		set_anim_rotation(anim_rotation)
 
 
 func set_anim_rotation(radians: float) -> void:
@@ -27,9 +28,21 @@ func get_anim_rotation() -> float:
 	return result
 
 
+func start_animation() -> void:
+	_anim.show()
+	_anim.play()
+
+
 func _on_AnimatedSprite_animation_finished() -> void:
-	if _sound.playing:
+	_anim.stop()
+	_anim.hide()
+
+	if wait_for_sound and _sound.playing:
 		yield(_sound, "finished")
 
 	emit_signal("finished")
+
+	if _sound.playing:
+		yield(_sound, "finished")
+
 	queue_free()
