df4a6c08034080d9903deb211e78a6946ae96677
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2021-04-04 23:08:11 -0400

    Finish push actor effect

--- src/skills/effects/PushAttackTargetEffect.gd
+++ src/skills/effects/PushAttackTargetEffect.gd
@@ -1,7 +1,9 @@
 class_name PushAttackTargetEffect, "res://assets/editor/pushattack_effect.png"
 extends SkillEffect
 
-const _BLOCK_ATTACK_MULT := 2
+const _SPEED := 7.0 # Cells per second
+const _LAND_HIT_DIST := 0.25
+const _BLOCK_HIT_DIST := 0.5
 
 export var max_distance := 1
 export var block_damages_allies_only := true
@@ -13,7 +15,7 @@ func get_aoe(target_cell: Vector2, source_cell: Vector2,
 	var direction := _get_push_direction(actor, source_cell)
 	var real_distance := _get_real_distance(actor, map, direction)
 
-	var end_cell := actor.origin_cell + (direction * real_distance)
+	var end_cell := _get_end_cell(actor, direction, real_distance)
 	var blocking_actors := _get_blocking_actors(actor, map, direction, end_cell)
 	if not blocking_actors.empty():
 		end_cell += direction
@@ -25,7 +27,34 @@ func get_aoe(target_cell: Vector2, source_cell: Vector2,
 
 func predict_damage(target_cell: Vector2, source_cell: Vector2,
 		source_actor: Actor, map: Map) -> Dictionary:
-	return {}
+	var result := {}
+
+	var actor := map.get_actor_on_cell(target_cell)
+	var direction := _get_push_direction(actor, source_cell)
+	var real_distance := _get_real_distance(actor, map, direction)
+
+	var end_cell := _get_end_cell(actor, direction, real_distance)
+	var blocking_actors := _get_blocking_actors(actor, map, direction, end_cell)
+
+	var attack := source_actor.stats.attack
+	if not blocking_actors.empty():
+		attack *= 2
+	var damage := actor.stats.damage_from_attack(attack)
+	result[actor] = -damage
+
+	for a in blocking_actors:
+		var blocking_actor := a as Actor
+		var other_damage := blocking_actor.stats.damage_from_attack(
+				source_actor.stats.attack)
+		assert(not result.has(blocking_actor))
+		result[blocking_actor] = -other_damage
+
+	return result
+
+
+static func _get_end_cell(actor: Actor, direction: Vector2, distance: int) \
+		-> Vector2:
+	return actor.origin_cell + (direction * distance)
 
 
 func _run_self(target_cell: Vector2, source_cell: Vector2,
@@ -34,17 +63,49 @@ func _run_self(target_cell: Vector2, source_cell: Vector2,
 	var direction := _get_push_direction(actor, source_cell)
 
 	var real_distance := _get_real_distance(actor, map, direction)
-	var end_cell := actor.origin_cell + (direction * real_distance)
+	var end_cell := _get_end_cell(actor, direction, real_distance)
 
-	var attack := source_actor.stats.attack
+	assert(map.actor_can_enter_cell(actor, end_cell))
+
+	actor.stats.take_damage(source_actor.stats.attack, direction, false)
+
+	var main_offset := end_cell - actor.origin_cell
+
+	var offset := main_offset
 	if real_distance < max_distance:
-		attack *= _BLOCK_ATTACK_MULT
+		offset += direction * _BLOCK_HIT_DIST
+	elif actor.is_alive:
+		offset += direction * _LAND_HIT_DIST
 
-	actor.receive_attack(attack, direction)
-	actor.origin_cell = end_cell
+	yield(
+		actor.animate_offset(
+			offset, real_distance / _SPEED,
+			Tween.TRANS_QUAD, Tween.EASE_IN_OUT),
+		"completed"
+	)
 
 	var other_attack_waiter := _hit_blocking_actors(actor, map, direction,
-			end_cell, source_actor.stats.attack)
+		end_cell, source_actor.stats.attack)
+
+	if actor.is_alive and (real_distance < max_distance):
+		actor.stats.take_damage(source_actor.stats.attack, -direction, false)
+
+	actor.origin_cell = end_cell
+	actor.cell_offset -= main_offset
+
+	if actor.is_alive:
+		assert(actor.cell_offset != Vector2.ZERO)
+		yield(
+			actor.animate_offset(
+				Vector2.ZERO, _BLOCK_HIT_DIST / _SPEED,
+				Tween.TRANS_QUAD, Tween.EASE_OUT),
+			"completed"
+		)
+	elif real_distance < max_distance:
+		actor.animate_death(-direction)
+	else:
+		actor.animate_death(direction)
+
 	if actor.animating:
 		yield(actor, "animation_finished")
 	if other_attack_waiter.waiting:
@@ -98,9 +159,7 @@ func _hit_blocking_actors(actor: Actor, map: Map, direction: Vector2,
 	var other_actors := _get_blocking_actors(actor, map, direction, end_cell)
 	for a in other_actors:
 		var other_actor := a as Actor
-		waiter.wait_for_signal(
-			other_actor.receive_attack(attack, direction),
-			"completed"
-		)
+		other_actor.stats.take_damage(attack, direction)
+		waiter.wait_for_signal(other_actor, "animation_finished")
 
 	return waiter
