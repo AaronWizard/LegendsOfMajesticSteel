cc2cdd771b0b4348c719e9cd26fbb0c4199a095a
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2021-04-21 19:50:02 -0400

    Don't spawn projectile effects if start and end are the same

--- src/core/skills/effects/ProjectileEffect.gd
+++ src/core/skills/effects/ProjectileEffect.gd
@@ -7,19 +7,20 @@ export var target_is_actor := false
 
 func _run_self(target_cell: Vector2, source_cell: Vector2,
 		source_actor: Actor, map: Map):
-	var projectile := projectile_scene.instance() as Projectile
-	map.add_effect(projectile)
+	if source_cell != target_cell:
+		var projectile := projectile_scene.instance() as Projectile
+		map.add_effect(projectile)
 
-	if source_cell == source_actor.origin_cell:
-		projectile.start.copy(source_actor)
-	else:
-		projectile.start.origin_cell = source_cell
+		if source_cell == source_actor.origin_cell:
+			projectile.start.copy(source_actor)
+		else:
+			projectile.start.origin_cell = source_cell
 
-	if target_is_actor:
-		var target_actor := map.get_actor_on_cell(target_cell)
-		projectile.end.copy(target_actor)
-	else:
-		projectile.end.origin_cell = target_cell
+		if target_is_actor:
+			var target_actor := map.get_actor_on_cell(target_cell)
+			projectile.end.copy(target_actor)
+		else:
+			projectile.end.origin_cell = target_cell
 
-	projectile.start_animation()
-	yield(projectile, "finished")
+		projectile.start_animation()
+		yield(projectile, "finished")
