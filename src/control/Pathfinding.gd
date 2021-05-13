f453c22b33321b625e22b0ebd6cf435f34341d81
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2021-05-12 22:01:52 -0400

    Fix move range calculation

--- src/control/Pathfinding.gd
+++ src/control/Pathfinding.gd
@@ -1,6 +1,6 @@
 class_name Pathfinding
 
-# Ignores allied actors
+# Dijkstra's Algorithm
 static func find_move_range(actor: Actor, map: Map) -> Array:
 	var result := {}
 
@@ -8,8 +8,9 @@ static func find_move_range(actor: Actor, map: Map) -> Array:
 	var queue := [ actor.origin_cell ]
 
 	while not queue.empty():
-		var c = queue.pop_front()
-		var current_cell := c as Vector2
+		var current_cell_index := _next_cell_index(queue, costs)
+		var current_cell := queue[current_cell_index] as Vector2
+		queue.remove(current_cell_index)
 
 		result[current_cell] = true
 
@@ -19,14 +20,33 @@ static func find_move_range(actor: Actor, map: Map) -> Array:
 			var adj_cost := map.get_cell_move_cost(adj_cell, actor)
 			adj_cost += costs[current_cell] as int
 
-			if adj_cost <= actor.stats.move \
-					and (not result.has(adj_cell) or (adj_cost < costs[adj_cell])):
-				queue.push_back(adj_cell)
-				costs[adj_cell] = costs[current_cell] + adj_cost
+			if adj_cost <= actor.stats.move and ( \
+					not costs.has(adj_cell) or (adj_cost < costs[adj_cell]) ):
+				costs[adj_cell] = adj_cost
+				if queue.find(adj_cell) == -1:
+					queue.append(adj_cell)
 
 	return result.keys()
 
 
+static func _next_cell_index(queue: Array, costs: Dictionary) -> int:
+	var result := 0
+	var result_cell := queue[result] as Vector2
+	var result_cost := costs[result_cell] as int
+
+	for i in range(1, queue.size()):
+		var index := i as int
+		var other_cell := queue[index] as Vector2
+		var other_cost := costs[other_cell] as int
+
+		if other_cost < result_cost:
+			result = index
+			result_cost = other_cost
+
+	return result
+
+
+# Ignores allied actors
 static func _adjacent_cells(cell: Vector2, actor: Actor, map: Map) -> Array:
 	var result := []
 
