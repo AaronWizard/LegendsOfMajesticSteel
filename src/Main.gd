ea6d980eedffbecbc9452ba2c46fab0b268a2fc1
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2021-08-22 18:43:03 -0400

    Screen transitions for title screen

--- src/Main.gd
+++ src/Main.gd
@@ -2,7 +2,16 @@ extends Node
 
 const _GAME_SCENE_PATH := "res://src/game/Game.tscn"
 
+onready var _screen_transition := $ScreenTransition as ScreenTransition
+
+
+func _ready() -> void:
+	_screen_transition.fade_in()
+	yield(_screen_transition, "faded_in")
+
 
 func _on_Start_pressed() -> void:
+	_screen_transition.fade_out()
+	yield(_screen_transition, "faded_out")
 	# warning-ignore:return_value_discarded
 	get_tree().change_scene(_GAME_SCENE_PATH)
