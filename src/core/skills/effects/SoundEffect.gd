031732dfdb76faf66f43f511c575df9364856c3b
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2021-09-17 19:51:10 -0400

    Skill sound effect

---
+++ src/core/skills/effects/SoundEffect.gd
@@ -0,0 +1,16 @@
+class_name SoundEffect, "res://assets/editor/sound_effect.png"
+extends SkillEffect
+
+export var stream: AudioStream
+
+
+func _run_self(_target_cell: Vector2, _source_cell: Vector2,
+		_source_actor: Actor, map: Map):
+	var stream_player := AudioStreamPlayer.new()
+	stream_player.stream = stream
+	map.add_child(stream_player)
+	
+	stream_player.play()
+	yield(stream_player, "finished")
+	
+	map.remove_child(stream_player)
