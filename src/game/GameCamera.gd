255e076483bdb2725c0a9825e0662363fda4ffa5
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2021-10-16 22:43:49 -0400

    Manually control camera movement with tween to prevent errors and have better control

--- src/game/GameCamera.gd
+++ src/game/GameCamera.gd
@@ -1,6 +1,10 @@
 class_name GameCamera
 extends Camera2D
 
+var _MOVE_TIME := 0.25
+
+onready var _tween := $Tween as Tween
+
 var _current_actor: Actor = null
 var _bounds := Rect2()
 
@@ -17,6 +21,7 @@ func set_bounds(rect: Rect2) -> void:
 
 func drag(relative: Vector2) -> void:
 	stop_following_actor()
+	_stop_moving()
 
 	var new_position := position - relative
 
@@ -34,7 +39,7 @@ func drag(relative: Vector2) -> void:
 
 func follow_actor(actor: Actor) -> void:
 	stop_following_actor()
-
+	_stop_moving()
 	_set_smoothing(true, true)
 
 	_current_actor = actor
@@ -43,8 +48,20 @@ func follow_actor(actor: Actor) -> void:
 
 func move_to_position(new_position: Vector2, drag_margin := true) -> void:
 	stop_following_actor()
-	_set_smoothing(true, drag_margin)
-	position = new_position
+
+	_stop_moving()
+
+	# Don't use built-in screen smoothing. Camera somehow stops moving in
+	# certain conditions and the extra control over the camera movement is
+	# useful anyway.
+	if not drag_margin or _need_move(new_position):
+		# warning-ignore:return_value_discarded
+		_tween.interpolate_property(self, "position", null, new_position,
+				_MOVE_TIME, Tween.TRANS_QUAD, Tween.EASE_OUT)
+		# warning-ignore:return_value_discarded
+		_tween.start()
+
+		yield(_tween, "tween_all_completed")
 
 
 func stop_following_actor() -> void:
@@ -54,6 +71,40 @@ func stop_following_actor() -> void:
 
 	_set_smoothing(false, false)
 	position = get_camera_position()
+
+
+# For some reason toggling the built-in smoothing and dragging makes the
+# camera unable to change its position. Specifically when you move a player
+# actor, cancel its turn, and drag the mouse while the actor is moving back to
+# its original position.
+# Therefore dragging is being handled manually. See move_to_position.
+func _need_move(new_position: Vector2) -> bool:
+	var result := true
+
+	var viewport := get_viewport()
+	if viewport:
+		var diff := new_position - position
+		var screen_new_pos := get_global_transform_with_canvas().origin + diff
+
+		var view_size := viewport.size
+		var margin_left := view_size.x * drag_margin_left
+		var margin_right := view_size.x * drag_margin_right
+		var margin_top := view_size.y * drag_margin_top
+		var margin_bottom := view_size.y * drag_margin_bottom
+
+		var margin_start := Vector2(margin_left, margin_top)
+		var margin_size := viewport.size - margin_start \
+				- Vector2(margin_right, margin_bottom)
+
+		var rect := Rect2(margin_start, margin_size)
+		result = not rect.has_point(screen_new_pos)
+
+	return result
+
+
+func _stop_moving() -> void:
+	# warning-ignore:return_value_discarded
+	_tween.stop(self, "position")
 
 
 func _set_smoothing(smoothing: bool, drag_margin: bool) -> void:
