597cb9c01f76ed6bae89af7d08021f3b03050287
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2021-11-06 00:07:00 -0400

    Made conditions panel smaller

--- src/ui/battle/actordetails/SingleConditionDetails.gd
+++ src/ui/battle/actordetails/SingleConditionDetails.gd
@@ -1,13 +1,15 @@
 class_name SingleConditionDetails
-extends VBoxContainer
+extends HBoxContainer
 
-const _ROUNDS_LEFT_TEXT := "%s rounds"
+const _ROUND_LEFT_SINGLE := "1 round"
+const _ROUNDS_LEFT_PLURAL := "%s rounds"
 
-onready var _icon := $ConditionDescription/Icon as TextureRect
-onready var _name := $ConditionDescription/Name as Label
-onready var _magnitude := $ConditionDescription/Magnitude as Label
+onready var _icon := $Icon as TextureRect
+onready var _name := $Name as Label
+onready var _magnitude := $Magnitude as Label
 
-onready var _rounds_left := $ConditionValues/RoundsLeft as Label
+onready var _divider := $Divider as Control
+onready var _rounds_left := $RoundsLeft as Label
 
 
 func set_condition_icon(value: Texture) -> void:
@@ -26,9 +28,15 @@ func set_magnitude(magnitude: int, is_percentage := false) -> void:
 
 
 func show_rounds_left(rounds_left: int) -> void:
+	_divider.visible = true
 	_rounds_left.visible = true
-	_rounds_left.text = _ROUNDS_LEFT_TEXT % rounds_left
+
+	if rounds_left == 0:
+		_rounds_left.text = _ROUND_LEFT_SINGLE
+	else:
+		_rounds_left.text = _ROUNDS_LEFT_PLURAL % rounds_left
 
 
 func hide_rounds_left() -> void:
+	_divider.visible = false
 	_rounds_left.visible = false
