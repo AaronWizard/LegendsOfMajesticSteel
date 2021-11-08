ad7d42a3ab315c302ad872c858b255e5bf6a9752
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2021-11-08 16:39:49 -0500

    Use existing Actor.animate_offset method in move effect

--- src/core/skills/effects/MoveSelfToTargetEffect.gd
+++ src/core/skills/effects/MoveSelfToTargetEffect.gd
@@ -3,14 +3,6 @@ extends SkillEffect
 
 export var speed := 0.0 # Tiles per second
 
-var _tween: Tween
-
-
-func _ready() -> void:
-	if speed > 0:
-		_tween = Tween.new()
-		add_child(_tween)
-
 
 func _run_self(target_cell: Vector2, _source_cell: Vector2,
 		source_actor: Actor, map: Map) -> void:
@@ -23,11 +15,8 @@ func _run_self(target_cell: Vector2, _source_cell: Vector2,
 		var offset := target_cell - source_actor.origin_cell
 		var time := offset.length() / speed
 		source_actor.set_facing(offset)
-		# warning-ignore:return_value_discarded
-		_tween.interpolate_property(source_actor, "cell_offset", Vector2.ZERO,
-				offset, time, Tween.TRANS_QUAD, Tween.EASE_OUT)
-		# warning-ignore:return_value_discarded
-		_tween.start()
-		yield(_tween, "tween_all_completed")
+
+		yield(source_actor.animate_offset(offset, time,
+				Tween.TRANS_QUAD, Tween.EASE_OUT), "completed")
 		source_actor.cell_offset = Vector2.ZERO
 	source_actor.origin_cell = target_cell
