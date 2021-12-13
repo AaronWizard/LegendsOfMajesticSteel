1e6a62f86840accbfd4858098a536570e1713358
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2021-12-13 02:05:09 -0500

    Sound and music audio buses

--- src/core/skills/effects/SoundEffect.gd
+++ src/core/skills/effects/SoundEffect.gd
@@ -8,10 +8,12 @@ func _run_self(_target_cell: Vector2, _source_cell: Vector2,
 		_source_actor: Actor, map: Map):
 	var stream_player := AudioStreamPlayer.new()
 	stream_player.stream = stream
+	stream_player.bus = "Sounds"
+
 	map.add_child(stream_player)
-	
+
 	stream_player.play()
 	yield(stream_player, "finished")
-	
+
 	map.remove_child(stream_player)
 	stream_player.queue_free()
