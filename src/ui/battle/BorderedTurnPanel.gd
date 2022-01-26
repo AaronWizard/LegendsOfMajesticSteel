02d52bfeb404a6aa8b24ea174466192b89e96ba4
AaronWizard <33398493+AaronWizard@users.noreply.github.com>
2022-01-25 19:52:26 -0500

    Only animate turn panel border if turn panel changed

--- src/ui/battle/BorderedTurnPanel.gd
+++ src/ui/battle/BorderedTurnPanel.gd
@@ -11,16 +11,9 @@ onready var _scroll := $CenterContainer/PanelContainer/ScrollContainer \
 		as ScrollContainer
 onready var _tween := $Tween as Tween
 
+
 func get_turn_panel() -> TurnPanel:
 	return _turn_panel
-
-
-func _resize_border() -> void:
-	var border := $CenterContainer/PanelContainer as Control
-	var new_min_size := _new_min_size()
-	if border.rect_min_size != new_min_size:
-		border.rect_min_size = new_min_size
-		border.rect_size = Vector2.ZERO
 
 
 func _new_min_size() -> Vector2:
@@ -44,11 +37,32 @@ func _scroll_to_turn_slot() -> void:
 
 
 func _on_BorderedTurnPanel_resized() -> void:
-	_resize_border()
+	var border := $CenterContainer/PanelContainer as Control
+	var new_min_size := _new_min_size()
+	if border.rect_min_size != new_min_size:
+		border.rect_min_size = new_min_size
+		border.rect_size = Vector2.ZERO
 
 
 func _on_TurnPanel_minimum_size_changed() -> void:
-	_resize_border()
+	var border := $CenterContainer/PanelContainer as Control
+	var tween := $Tween as Tween
+
+	var new_min_size := _new_min_size()
+	if border.rect_min_size != new_min_size:
+		var old_size := border.rect_size
+
+		border.rect_min_size = new_min_size
+		border.rect_size = Vector2.ZERO
+
+		var new_size := border.rect_size
+
+		border.rect_size = old_size
+		# warning-ignore:return_value_discarded
+		tween.interpolate_property(border, "rect_size",
+			old_size, new_size, TurnPanel.ANIM_TIME)
+		# warning-ignore:return_value_discarded
+		tween.start()
 
 
 func _on_TurnPanel_turn_advanced() -> void:
